282082a1a8c45e70714db5135a1bc5a78ee69d2a
kithkui <kithkui@kithkuis-MacBook-Air.local>
2024-12-16 18:00:44 -0300

    aloja

--- utils/farcaster.ts
+++ utils/farcaster.ts
@@ -193,7 +193,10 @@ export async function castClankerWithTokenInfo(
 
   async function attemptReply(attempt = 1): Promise<string> {
     try {
-      const cast_text = `@tinybot deploy $${ticker}: "${token_name.toLowerCase()}":\n\n${description}`;
+      const cast_text = `@tinybot deploy $${ticker}: "${token_name.toLowerCase()}":\n\n${description.replace(
+        "anky",
+        ticker
+      )}`;
       const trimmed_cast_text =
         cast_text.slice(0, 1000) + "...".slice(0, 1020) + "...";
       const options = {
