a71342d8587317c9deee52d07d8f62171bd055d1
kithkui <kithkui@kithkuis-MacBook-Air.local>
2025-01-14 22:02:04 -0300

    aloja

--- src/routes/weeklyhackathon/functions.ts
+++ src/routes/weeklyhackathon/functions.ts
@@ -135,6 +135,8 @@ export async function createFramedImageWithMask({
           {
             headers: {
               Authorization: `Client-ID fab07e0ec58d514`,
+              "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
             },
           }
         );
@@ -144,7 +146,12 @@ export async function createFramedImageWithMask({
         const directImageUrl = response.data.data.link;
 
         // Download the image using the direct URL
-        const imageResponse = await fetch(directImageUrl);
+        const imageResponse = await fetch(directImageUrl, {
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+          },
+        });
         if (!imageResponse.ok) {
           throw new Error(
             `Failed to fetch imgur image: ${imageResponse.status}`
