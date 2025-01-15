73336451729efcf1011f41ca7bc7c237f14baa04
kithkui <kithkui@kithkuis-MacBook-Air.local>
2025-01-14 23:04:14 -0300

    aloja

--- src/routes/weeklyhackathon/functions.ts
+++ src/routes/weeklyhackathon/functions.ts
@@ -97,9 +97,27 @@ async function downloadImgurImage(url: string): Promise<string> {
       url: url,
       responseType: "stream",
       headers: {
-        Authorization: `Client-ID fab07e0ec58d514`,
+        // Spoof common browser headers
         "User-Agent":
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+        "Cache-Control": "max-age=0",
+        "Sec-Ch-Ua":
+          '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        Referer: "https://imgur.com/",
+        Origin: "https://imgur.com",
+        DNT: "1",
       },
     });
 
@@ -120,7 +138,6 @@ async function downloadImgurImage(url: string): Promise<string> {
     return tempPath;
   } catch (error) {
     console.error("Error downloading imgur image");
-
     throw error;
   }
 }
