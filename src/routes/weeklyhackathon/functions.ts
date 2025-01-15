c9bb435cc7b4930f7f70400e11ca5df9a43e4d4b
kithkui <kithkui@kithkuis-MacBook-Air.local>
2025-01-14 22:51:51 -0300

    aloja

--- src/routes/weeklyhackathon/functions.ts
+++ src/routes/weeklyhackathon/functions.ts
@@ -96,6 +96,11 @@ async function downloadImgurImage(url: string): Promise<string> {
       method: "get",
       url: url,
       responseType: "stream",
+      headers: {
+        Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+      },
     });
 
     console.log("Got response from imgur");
